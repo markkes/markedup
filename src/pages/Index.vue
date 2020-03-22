5f34d62119f4f09e76bead7be32a6a21aa7d6372
Mark Feenstra <mark@synetic.nl>
2020-03-21 22:08:39 -0500

    setup homepage and color scheme

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,15 +4,14 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1>Hallo!</h1>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Ik ben Mark Feenstra, eigenaar van Markedup. Geboren en getogen in Heemskerk ben ik van jongs af aan al geinstresseerd in computers. Na mijn middelbare school ontwikkelde deze intresse zich langzaam aan richting het web. Sindsdien heb ik aan veel diverse creative projecten mee mogen werken. Momenteel ben ik in te huren als Freelance en ben ik gespecialiseerd in het crearen van toffe Front-end Systemen.
     </p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    <p>
+      Vindt mij als @markkes op <a href="www.linkedin.com/in/mfeenstra86" title="LinkedIn">LinkedIn</a>, <a href="https://linkedin.com/markkes" title="LinkedIn">LinkedIn</a> & <a href="https://github.com/markkes" title="Github">Github</a>.
     </p>
 
   </Layout>
@@ -27,7 +26,25 @@ export default {
 </script>
 
 <style>
+
+:root {
+  --primary-color: #00a292;
+  --secondary-color: #bdbdbd;
+  --light-color: #FFF;
+}
+
+body {
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+}
+
 .home-links a {
   margin-right: 1rem;
 }
+
+a {
+  color: var(--light-color);
+  text-decoration: none;
+}
+
 </style>
