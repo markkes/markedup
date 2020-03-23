c97be576f2c23598ab1961cc2677dde7378b807f
Mark Feenstra <mark@synetic.nl>
2020-03-22 23:32:24 -0500

    removed custom styling

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <div class="layout bg-white border-blue-100">
     <header class="flex">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
@@ -29,56 +29,5 @@ query {
 </static-query>
 
 <style>
-
-:root {
-  --theme-color: #00a292;
-  --theme-light-color: #c8d4d3;
-  --theme-dark-color: #004c45;
-  --bg-color: #FFFFFF;
-  --text-color: #616968;
-  --link-color: #000000;
-  --white-color: #FFFFFF;
-}
-
-html,
- body {
-  background-color: var(--bg-color);
-  color: var(--text-color);
-}
-
-html {
-  border: 20px solid var(--theme-color);
-}
-
-body {
-  background-color: var(--white-color);
-}
-
-a {
-  color: var(--link-color);
-  text-decoration: none;
-  transition: transform ease-in-out .3s, background-size ease-in-out .3s, color ease-in-out .3s;
-  text-decoration: none;
-  background-image: linear-gradient(var(--theme-color), var(--theme-color));
-  background-position: 0% 100%;
-  background-repeat: no-repeat;
-  background-size: 0% 2px;
-}
-
-a:hover {
-  background-size: 100% 2px;
-  color: var(--theme-color);
-}
-
-p a {  
-  background-size: 0% 100%;
-  background-position: 50% 100%;
-}
-
-p a:hover {
-  background-size: 100% 100%;
-  color: var(--white-color);
-
-}
 
 </style>
